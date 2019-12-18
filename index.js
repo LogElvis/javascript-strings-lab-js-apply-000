361638b3f78959b5a23d07a4ba4202ff90d1e6f0
awesome-partial-8458 <ningelvis@gmail.com>
2019-12-18 20:16:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var greeting = "Hello, everybody!";
 
+<<<<<<< HEAD
 var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
@@ -7,3 +8,6 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 var topic = "space";
 
 var conversation = `Let's talk about ${topic}.`;
+=======
+var specialGuest = "Hello
+>>>>>>> 9779f54e4095dd0625cfa35ecac9dc71126d6398
